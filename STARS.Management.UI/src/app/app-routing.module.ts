b3a88381266701df069c70ad2bb4ac7771b3bb34
vaibhavjoshi888 <vaibhavjoshi888@gmail.com>
2022-10-12 13:10:27 +0530

    added  welcome compoenent

--- STARS.Management.UI/src/app/app-routing.module.ts
+++ STARS.Management.UI/src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { AuthGuard } from './_helpers/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'welcome', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
+
 
 export const appRoutingModule = RouterModule.forRoot(routes);
--- STARS.Management.UI/src/app/welcome/welcome.component.spec.ts
+++ STARS.Management.UI/src/app/welcome/welcome.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthLoginComponent } from './auth-login.component';
+import { WelcomeComponent } from './welcome.component';
 
-describe('AuthLoginComponent', () => {
-  let component: AuthLoginComponent;
-  let fixture: ComponentFixture<AuthLoginComponent>;
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthLoginComponent ]
+      declarations: [ WelcomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AuthLoginComponent);
+    fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
