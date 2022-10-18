a9472768a30f7892c634f2f59047baf9a8dcc82f
vaibhavjoshi888 <vaibhavjoshi888@gmail.com>
2022-10-19 00:04:58 +0530

    data binded

--- STARS.Management.UI/src/app/shared/shared.module.ts
+++ STARS.Management.UI/src/app/shared/shared.module.ts
@@ -4,8 +4,6 @@ import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
-
-
 @NgModule({
   declarations: [MenuComponent,FooterComponent],
   imports: [
